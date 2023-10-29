fbf9184956cadd1a9870025e6eb680f3a0a8fac2
Deity-X <143567776+Deity-X@users.noreply.github.com>
2023-10-29 14:35:37 -0500

    Update Garreth_Salang_R11744065_Project1.c

--- Garreth_Salang_R11744065_Project1.c
+++ Garreth_Salang_R11744065_Project1.c
@@ -522,7 +522,7 @@ int main(int argc, char* argv[])
 {
   printf("DCooke Analyzer :: R11744065 \n");
   /* Open the input data file and process its contents */
-  if ((in_fp = fopen("ginput_0.txt, "r")) == NULL) 
+  if ((in_fp = fopen("ginput_0.txt", "r")) == NULL) 
   {
     printf("ERROR - cannot open front.in \n");
   } 
